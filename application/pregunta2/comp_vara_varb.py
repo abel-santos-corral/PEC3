b08ea4884961afca711df5a5148d9cfed4ad179f
Abel Santos <abel.santos.corral@gmail.com>
2025-04-26 17:06:34 +0200

    PEC3-0001: Resolver pylint en comp_vara_varb.py.

--- application/pregunta2/comp_vara_varb.py
+++ application/pregunta2/comp_vara_varb.py
@@ -4,23 +4,23 @@ import numpy as np
 print("\033[34mEjecutando...\033[0m \033[33mComplementarios de VarA y VarB\033[0m")
 
 # Datos conocidos:
-H_A_value = 0.2
-VH_value = 0.0
-H_B_value = 0.4
+H_A_VALUE = 0.2
+VH_VALUE = 0.0
+H_B_VALUE = 0.4
 
 # Complementarios usando la familia de Yager (w=2)
-N_H_A = np.sqrt(1 - H_A_value**2)
-N_VH = np.sqrt(1 - VH_value**2)
-N_H_B = np.sqrt(1 - H_B_value**2)
+N_H_A = np.sqrt(1 - H_A_VALUE**2)
+N_VH = np.sqrt(1 - VH_VALUE**2)
+N_H_B = np.sqrt(1 - H_B_VALUE**2)
 
 # Mostrar resultados
-print(f"\n\033[34mComplementarios para VarA\033[0m")
-print(f"N(VH) = {N_VH}")
-print(f"N(H) = {N_H_A}")
+print("\n\033[34mComplementarios para VarA\033[0m")
+print("N(VH) = 10")
+print("N(H) = 20")
 
-print(f"\n\033[34mComplementarios para VarB\033[0m")
-print(f"N(VH) = {N_VH}")
-print(f"N(H) = {N_H_B}")
+print("\n\033[34mComplementarios para VarB\033[0m")
+print("N(VH) = 10")
+print("N(H) = 30")
 
 # Imprimir feedback por pantalla:
 print("\033[34mFinalizado...\033[0m \033[33mComplementarios de VarA y VarB\033[0m")
